d7ef9af10b4fbc527b81dd785d911f7e108b89e2
unknown <phsu2021@sju.ac.kr>
2019-05-31 00:21:33 +0900

    init

---
+++ Client/DualRoomChat/DualRoomChat.h
@@ -0,0 +1,32 @@
+﻿
+// DualRoomChat.h: PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CDualRoomChatApp:
+// 이 클래스의 구현에 대해서는 DualRoomChat.cpp을(를) 참조하세요.
+//
+
+class CDualRoomChatApp : public CWinApp
+{
+public:
+	CDualRoomChatApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDualRoomChatApp theApp;
